9a70293207f86d71a08ae3449488a8b9b7deca6d
rahimi.ali <ali.saee@outlook.de>
2025-09-23 08:52:18 +0200

    3.nodejs - embedding approach (relationships between collections in mongodb)-II

--- relation.js
+++ relation.js
@@ -16,14 +16,16 @@ mongoose
 const User = mongoose.model('User', new mongoose.Schema({
   first_name: String,
   last_name: String,
-  book: bookSchema
+  // book: bookSchema
+  books: [bookSchema]
 }));
 
-async function createUser(first_name, last_name, book){
+async function createUser(first_name, last_name, books){
   const user = new User({
     first_name,
     last_name, 
-    book
+    // book: book
+    books: books
   });
 
   const result = await user.save();
@@ -66,6 +68,29 @@ async function updateUser(id){
   });
 }
 
- updateUser('68d1841f529b2a212cc1601a')
+async function addBook(userId, book){
+  const user = await User.findById(userId);
+  user.books.push(book);
+  await user.save();
+}
+  
+async function removeBook(userId, bookId){
+  const user = await User.findById(userId);
+  const book = user.books.id(bookId);
+  book.deleteOne();
+  await user.save();
+}
+
+removeBook('68d19f5f1cad6892f1f05441','68d23e56cc2aee2121f9277b');
+
+// addBook('68d19f5f1cad6892f1f05441', new Book({title: 'Expressjs', pages: 400}));
+
+  // createUser('Ali', 'Rahimi', [
+  //   new Book({title: 'nodejs programming', pages: 500}),
+  //   new Book({title: 'react programming', pages: 600}),
+  //   new Book({title: 'mongodb', pages: 200})
+  // ]);
+
+ // updateUser('68d1841f529b2a212cc1601a')
  // createUser('Ali', 'Rahimi', new Book({title: 'nodejs programming', pages: 500}));
  // getUsers();
